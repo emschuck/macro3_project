953c8d55da177578d22a656fd78dffa1a666366d
“Elizabeth <emschuck@github.com>
2026-05-25 12:41:11 +0200

    Full code run

--- code/pipeline.R
+++ code/pipeline.R
@@ -13,39 +13,37 @@ rm(list = ls())
 # 1. Load constants
 # =====================================================
 
-source("code/00_constants.R", echo = FALSE)
+source("./code/00_constants.R", echo = FALSE)
 
 # =====================================================
 # 2. Download / refresh raw WDI data
 # =====================================================
 
-source("code/01_WDI_data_download.R", echo = FALSE)
+source("./code/01_WDI_data_download.R", echo = FALSE)
 
 # =====================================================
-# 3. Build processed panel
+# 3. Build processed panel 
 # =====================================================
 
-source("code/01_data_download.R", echo = FALSE)
+source("./code/01_data_download.R", echo = FALSE)
 
 # =====================================================
 # 4. Run main GDP SCM analysis
 # =====================================================
 
-source("code/02_replication.R", echo = FALSE)
+source("./code/02_replication.R", echo = FALSE)
 
-source("code/02_OLS_replication.R", echo = FALSE)
+source("./code/02_OLS_replication.R", echo = FALSE)
 
-source("code/02_Average_replication.R", echo = FALSE)
+source("./code/02_Average_replication.R", echo = FALSE)
 
 # =====================================================
 # 5. Run extension trade SCM analysis + placebo tests
 # =====================================================
 
-source("code/03_extension_implementation.R", echo = FALSE)
+source("./code/03_extension_implementation.R", echo = FALSE)
 
-source("code/03_extension_placebo.R", echo = FALSE)
-
-
+source("./code/03_extension_placebo.R", echo = FALSE)
 
 
 cat("\nFull pipeline complete.\n")
